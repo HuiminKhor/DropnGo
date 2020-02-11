b7f8c11487e4568aefd6cd40a5c02a2ef19b4522
HuiminKhor <k_huimin@hotmail.com>
2020-02-11 17:58:54 +0800

    booking function added

--- src/components/schedule2.js
+++ src/components/schedule2.js
@@ -1,13 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DateTimePicker,MuiPickersUtilsProvider, } from "@material-ui/pickers";
 import DateFnsUtils from '@date-io/date-fns';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { Typography } from '@material-ui/core';
+import moment from 'moment';
 
-function BasicDateTimePicker() {
+function BasicDateTimePicker(props) {
   const [dropOffDate, setDropOffDate] = useState(new Date());
   const [pickUpDate, setPickUpDate] = useState(new Date());
+  const [luggageNum, setLuggageNum] = useState(1)
+
+  const get_cost =()=>{
+      const start = moment(dropOffDate)
+      const end = moment(pickUpDate)
+      const duration = moment.duration(end.diff(start))
+      const hours = duration.asHours()
+      
+      console.log("hours:",hours)
+      let round = Math.floor(hours)
+      let cost;
+      if (hours > round){
+          cost = (round+1) * 5 * luggageNum
+      }
+      else{
+          cost = round * 5 *luggageNum
+      }
+      props.setCost(cost)
+      console.log("cost:",cost)
+  }
+  useEffect(() => {
+      get_cost()
+  }) 
+
+  const handleInput=(e)=>{
+      setLuggageNum(e.target.value)
+  }
+  
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -40,6 +69,8 @@ function BasicDateTimePicker() {
                     shrink: true,
                 }}
                 variant="outlined"
+                value={luggageNum}
+                onChange={handleInput}
         />
         </Grid>    
     </MuiPickersUtilsProvider>
