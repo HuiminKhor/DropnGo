323bf291bb102408e3896d83dd883ee58c215e48
Ben Hails <benhails@hotmail.com>
2020-02-12 18:00:12 +0800

    sorted routing and dynamic retrieval of stores after search from home page

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import CITIESANDCOUNTRIES from '../constants/CitiesAndCountries';
 import axios from 'axios';
 import { SearchBarDiv } from './Styled'
 import '../App.css'
-
+import { useHistory } from 'react-router-dom'
 
 
 const SearchBar = () => {
+    let history = useHistory();
 
-    let selectedLocation;
+    const [selectedLocation, setSelectedLocation] = useState({})
+
     const getStores = (e) => {
-        e.preventDefault()
-        if (selectedLocation) {
+        if (selectedLocation !== {}) {
             console.log(selectedLocation)
-            // axios request here to get matching stores from DB
-            axios({
-                method: 'GET',
-                url: `https://dropandgo.herokuapp.com/api/v1/stores?location=${selectedLocation.value}`,
-            })
-                .then(response => { // 'response' could be named anything here. It is simply capturing the return of the promise.
-                    console.log(response.data)
-                    // do something with the data returned
-                })
-                .catch(error => {
-                    console.error(error.response.data.message)
-                    // do something to deal with or show the error
-                })
+            e.preventDefault()
+            let path = `/luggage-storage/${selectedLocation.city}?index=${selectedLocation.value}`;
+            history.push(path);
         } else {
-            console.log("select a value first")
+            console.log("please select somewhere first")
         }
     }
+
 
     const getLocations = () => {
         const LOCATIONS = CITIESANDCOUNTRIES.map(location => {
@@ -41,20 +33,19 @@ const SearchBar = () => {
         })
         console.log(LOCATIONS)
         return LOCATIONS;
-
-
     }
+
+    const handleLocationChange = (location) => {
+        setSelectedLocation(location)
+      }
 
     return (
         <SearchBarDiv>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <Select className="searchbar" options={getLocations()} onChange={LOCATIONS => selectedLocation = LOCATIONS} />
-            <button onClick={getStores}><i className="fa fa-search"></i>
-</button>
+            <Select className="searchbar" options={getLocations()} onChange={handleLocationChange} /> 
+            <button onClick={getStores}><i className="fa fa-search"></i></button>
         </SearchBarDiv>
-
     )
-
 }
 
 export default SearchBar
