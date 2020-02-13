403558f2f1bdd360939ada2ac2b43c4b73852621
Chaltyr <louis21@hotmail.sg>
2020-02-13 18:16:33 +0800

    Committing now to rebase to get the new user function

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -6,6 +6,7 @@ import Qrgen from '../components/qrModal'
 import axios from 'axios'
 import '../App.css'
 
+import Moment from 'react-moment';
 
 const liStyle = {
     display: "flex",
@@ -44,6 +45,11 @@ const liStyle2 = {
 
 // book()
 
+// a = [1, 2, 3]
+// a.map(item => {
+//     return <p>{item}</p>
+// })
+
 
 const UserProfile = (id) => {
     const [modal, setModal] = useState(false);
@@ -52,15 +58,17 @@ const UserProfile = (id) => {
     const [mode, setMode] = useState(true)
 
     const [booking, setBooking] = useState({})
+    const [store, setStore] = useState({})
 
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/v1/bookings/${'1'}`)
+        // axios.get(`http://localhost:5000/api/v1/bookings/?user_id=2`)
+        axios.get(`https://dropandgo.herokuapp.com/api/v1/bookings?user_id=1`)
+
             .then(result => {
                 console.log(result)
-                this.setBooking(
-                    result.data
-                )
+                setBooking(result.data)
+                setStore(result.data.store)
             })
     
     }, [])
@@ -74,21 +82,8 @@ const UserProfile = (id) => {
             <div style={{textAlign:"center"}}>
                 <h2 className="profileBookingTitle">My Bookings</h2>
             </div>
-            {/* <div style={{display:"flex", justifyContent:"center", marginTop:"20px", marginBottom:"20px"}}>
-                <div style={btnStyle}>
-                    <div>
-                        <button className="allBookingButton" onClick={() => setMode(true)}>
-                            All Booking
-                        </button>                      
-                    </div>
-                    <div style={btnStyle2}>
-                        <button className="upcomingBookingButton" onClick={() => setMode(false)}>
-                            Upcoming Bookings
-                        </button>
-                    </div>
-                </div>
-            </div> */}
-            { mode? 
+            
+            { booking.map((book=>(
                 <div>
                 <Card style={{border:"1px solid #40739E", marginRight:"5vw", marginLeft:"5vw", paddingBottom:"20px", borderRadius:"10px", marginTop:"10px"}}>
                     <CardHeader style={{fontWeight:"bold", textAlign:"center", fontSize:"2rem"}}>Luggage Station at KL Sentral</CardHeader>
@@ -104,24 +99,24 @@ const UserProfile = (id) => {
                                     <div className="leftVariable">Operating hours</div>
                                 </div>
                                 <div scope="row" style={btnStyle2}>
-                                    <div className="rightVariable">Feb 6, 2020 17:15</div>
-                                    <div className="rightVariable">8 pm</div>
-                                    <div className="rightVariable">a</div>
-                                    <div className="rightVariable">b</div>
-                                    <div className="rightVariable">c</div>
-                                    <div className="rightVariable">7 eleven, KL SENTRAL </div>
-                                    <div className="rightVariable">Mon - sun, 09.15 - 22:00</div>
-                                </div>
-                                
-                            </div>
-                            
+                                    <div className="rightVariable">{booking.check_in_date_time}</div>
+                                    <div className="rightVariable">{booking.check_out_date_time}</div>
+                                    <div className="rightVariable">{booking.number_of_bag}</div>
+                                    <div className="rightVariable">{booking.status}</div>
+                                    <div className="rightVariable">{booking.price}</div>
+                                    <div className="rightVariable">{store.area}</div>
+                                    <div className="rightVariable">{store.opening_hours}</div>
+                                </div>                 
+                        </div>                           
                     </CardBody>
                     <div style={{textAlign:"center", marginTop:"20px"}}>
                         <Button style={{textAlign:"center", color:"#fff", background:"#40739E", border:"1px solid #40739E", width: "140px", fontWeight:"bold", fontSize:"1.5rem"}} onClick={toggle} >View Qr Code</Button>
                     </div>
-                    {/* <CardFooter style={{textAlign:"center"}}>Footer</CardFooter> */}
                 </Card>
-            </div> : <div>"anything here???</div>
+            </div>
+             ))) 
+             
+                
         }
         </div>
         
